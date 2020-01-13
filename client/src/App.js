63673275e67ca6887782dcd2de336c00ed90ef7c
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-01-13 11:40:54 -0500

    stubbing out App html

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,18 @@
 import React     from 'react'
 import { Route } from 'react-router-dom'
+import './styles/Reset.css'
+import './styles/App.css'
 
-const App = () => <div>react app</div>
+const App = () => <React.Fragment>
+                    <div className="hdr">
+
+                    </div>
+                    <div className="bdy">
+
+                    </div>
+                    <div className="ftr">
+                        
+                    </div>
+                </React.Fragment>
 
 export default App
